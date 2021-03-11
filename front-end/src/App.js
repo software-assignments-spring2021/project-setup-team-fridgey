0f66e68a0564deabec3427be0084691c4667f611
Alex <ah4605@stern.nyu.edu>
2021-03-11 15:02:02 -0500

    CSS Edits

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Fridgey </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Fridgey</p>
         <a
           className="App-link"
           href="https://reactjs.org"
