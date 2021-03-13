874e9b179e68847a377ca1a41854531359c958d8
Alex <ah4605@stern.nyu.edu>
2021-03-13 01:41:24 -0500

    MyFridge Draft 1

--- front-end/src/MyFridge.js
+++ front-end/src/MyFridge.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./MyFridge.css";
 import * as ReactBootStrap from "react-bootstrap";
 import { groups } from "./fridgeDataFake";
-import List from "./list";
+import { dot, chipDays } from "./dotColor";
 
 const MyFridge = (props) => {
   const renderItem = (data) => {
     return (
       <tr>
-        <td>{data.title}</td>
-        <td className="amountChip">{data.amount}</td>
-        <td>{data.daysLeft}</td>
+        <td>
+          {dot(data.daysLeft)}
+          {/* <dot days={data.daysLeft} /> */}
+          <span class="title">{data.title}</span>
+          <span>
+            <p class="chip"> {data.amount}</p>
+          </span>
+          <span>
+            {chipDays(data.daysLeft)}
+            {/* <p class="chip chip2"> {data.daysLeft + " Days"}</p> */}
+          </span>
+        </td>
       </tr>
     );
   };
@@ -19,16 +28,9 @@ const MyFridge = (props) => {
     <div>
       {groups.map((item) => (
         <div>
-          <ReactBootStrap.Table hover size="sm">
-            <h2 class="header">{item.header}</h2>
-            <tbody>{item.object.map(renderItem)}</tbody>
-          </ReactBootStrap.Table>
+          <h2 className="header">{item.header}</h2>
+          <table>{item.object.map(renderItem)}</table>
         </div>
-        // <List a={groups.header} b="text2" c="text3">
-        //   {/* <h2 class="header">{item.header}</h2>
-        //   <tbody>{item.object.map(renderItem)}</tbody> */}
-        // </List>
-        // <h2>{groups.header}</h2>
       ))}
     </div>
   );
@@ -36,6 +38,25 @@ const MyFridge = (props) => {
 
 // make this available to other modules as an import
 export default MyFridge;
+
+// const getDot = (days) => {
+//   if (days < 2) {
+//     return <span class="dot dotRed"></span>;
+//   } else if (days >= 2 && days < 6) {
+//     return <span class="dot dotYellow"></span>;
+//   } else {
+//     return <span class="dot dotGreen"></span>;
+//   }
+// };
+
+/*
+        <List className="List" a={item.object.map(renderItem)}>
+            { <h2 class="header">{item.header}</h2>
+          <tbody>{item.object.map(renderItem)}</tbody>}
+          </List>
+*/
+/* <td className="amountChip">{data.amount}</td>
+        <td>{data.daysLeft}</td> */
 
 /* <div>
 {groups.map((item) => (
--- front-end/src/fridgeDataFake.js
+++ front-end/src/fridgeDataFake.js
@@ -1,60 +1,23 @@
-// const fridgeDataFake = [
-//   (fruits = [
-//     {
-//       title: "Oranges",
-//       amount: "Lots",
-//       daysLeft: "10",
-//     },
-//     {
-//       title: "Bananas",
-//       amount: "Some",
-//       daysLeft: "5",
-//     },
-//     {
-//       title: "Pears",
-//       amount: "Few",
-//       daysLeft: "0",
-//     },
-//   ]),
-
-// groups = [
-//     {
-//       object: fruits,
-//       header: "Fruits",
-//     },
-//     {
-//       object: meats,
-//       header: "Meats",
-//     },
-//     {
-//       object: grains,
-//       header: "Grains",
-//     },
-//     {
-//       object: dairy,
-//       header: "Dairy",
-//     },
-//   ]
-
-// // make this available to other modules as an import
-
-// export default fridgeDataFake;
-
 const fruits = [
   {
     title: "Oranges",
     amount: "Lots",
-    daysLeft: "10",
+    daysLeft: 10,
   },
   {
     title: "Bananas",
     amount: "Some",
-    daysLeft: "5",
+    daysLeft: 50,
   },
   {
     title: "Pears",
     amount: "Few",
-    daysLeft: "0",
+    daysLeft: 0,
+  },
+  {
+    title: "Pears",
+    amount: "Few",
+    daysLeft: 0,
   },
 ];
 
@@ -128,3 +91,45 @@ export const groups = [
     header: "Dairy",
   },
 ];
+
+// const fridgeDataFake = [
+//   (fruits = [
+//     {
+//       title: "Oranges",
+//       amount: "Lots",
+//       daysLeft: "10",
+//     },
+//     {
+//       title: "Bananas",
+//       amount: "Some",
+//       daysLeft: "5",
+//     },
+//     {
+//       title: "Pears",
+//       amount: "Few",
+//       daysLeft: "0",
+//     },
+//   ]),
+
+// groups = [
+//     {
+//       object: fruits,
+//       header: "Fruits",
+//     },
+//     {
+//       object: meats,
+//       header: "Meats",
+//     },
+//     {
+//       object: grains,
+//       header: "Grains",
+//     },
+//     {
+//       object: dairy,
+//       header: "Dairy",
+//     },
+//   ]
+
+// // make this available to other modules as an import
+
+// export default fridgeDataFake;
