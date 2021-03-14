61fb0fc3bfd518f2581ee5fedcc705c3ddd42d78
cindykimxp <cindykim100@gmail.com>
2021-03-14 01:15:18 -0500

    Basic Storage Time Search Scrollable List
    1. Created function StorageTimeSearchList and used props to create the basic structure for every List within StorageTimeSearch groups (Fruits, Dairy, etc.)
    2. Created function Item that iterates through the elements in each StorageTimeSearchList and writes them out
    - Item-name is the name of the items (e.g. Apple, Pear)
    - Spoil-time is the recommended spoil time of that item
    - Default-time is the default spoil setting for that item
    3. Created new file StorageTimeSearchListGroups containing multiple functions, each representing the food groups and containing a list (with elements) of example items.
    4. Created Item.css that is the general css file for each Item.
    *Note: can't get the formatting right on Spoil-time and Default-time. Should be on top of each other.
    5. Reformatted using the Prettier package.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import "./StorageTimeSearch.css"
+import "./StorageTimeSearch.css";
+import "./Item.css";
+
+import {
+  StorageTimeSearchListFruits,
+  StorageTimeSearchListDairy,
+  StorageTimeSearchListGrain,
+  StorageTimeSearchListMeats,
+} from "./StorageTimeSearchListGroups";
+
 import NavBar from "./NavBar";
 import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Button from "@material-ui/core/Button";
@@ -11,15 +20,38 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/Storage-Time-Search" exact component={StorageTimeSearch}/>
-          <Route path="/Recommendations" component={Recommendations}/>
-          <Route path="/Storage-Time-Search/List" component={StorageTimeSearchList}/>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/Storage-Time-Search"
+            exact
+            component={StorageTimeSearch}
+          />
+          <Route path="/Recommendations" component={Recommendations} />
+          <Route
+            path="/Storage-Time-Search/List"
+            component={StorageTimeSearchList}
+          />
+          <Route
+            path="/Storage-Time-Search/ListFruits"
+            component={StorageTimeSearchListFruits}
+          />
+          <Route
+            path="/Storage-Time-Search/ListMeats"
+            component={StorageTimeSearchListMeats}
+          />
+          <Route
+            path="/Storage-Time-Search/ListDairy"
+            component={StorageTimeSearchListDairy}
+          />
+          <Route
+            path="/Storage-Time-Search/ListGrain"
+            component={StorageTimeSearchListGrain}
+          />
         </Switch>
       </Router>
     </div>
   );
-};
+}
 
 const Home = () => (
   <div>
@@ -42,18 +74,18 @@ const Home = () => (
 
 const StorageTimeSearch = () => (
   <div>
-    <NavBar/>
+    <NavBar />
     <header className="App-header">
       <h1>Storage Time Search</h1>
       <Grid container direction="row" justify="center" className="TopRow">
-        <Link to="/Storage-Time-Search/List">
+        <Link to="/Storage-Time-Search/ListFruits">
           <Grid item>
             <Button variant="contained" className="GridButton" id="Button1">
               Fruits
             </Button>
           </Grid>
         </Link>
-        <Link to="/Storage-Time-Search/List">
+        <Link to="/Storage-Time-Search/ListMeats">
           <Grid item>
             <Button variant="contained" className="GridButton">
               Meats
@@ -62,14 +94,14 @@ const StorageTimeSearch = () => (
         </Link>
       </Grid>
       <Grid container direction="row" justify="center">
-        <Link to="/Storage-Time-Search/List">
+        <Link to="/Storage-Time-Search/ListDairy">
           <Grid item>
             <Button variant="contained" className="GridButton" id="Button2">
               Dairy
             </Button>
           </Grid>
         </Link>
-        <Link to="/Storage-Time-Search/List">
+        <Link to="/Storage-Time-Search/ListGrain">
           <Grid item>
             <Button variant="contained" className="GridButton">
               Grain
@@ -81,18 +113,30 @@ const StorageTimeSearch = () => (
   </div>
 );
 
-const StorageTimeSearchList = () => (
-  <div>
-    <Link to="/Storage-Time-Search">Back</Link>
-    <header className="App-header">
-      <h1>LIST</h1>
-    </header>
-  </div>
-);
+export const StorageTimeSearchList = (props) => {
+  return (
+    <div>
+      <Link to="/Storage-Time-Search">Back</Link>
+      <h1>{props.title}</h1>
+    </div>
+  );
+};
+
+export const Item = ({ item, key }) => {
+  return (
+    <div>
+      <p className="Item">
+        <text className="Item-name">{item.name}.</text>
+        <text className="Spoil-time">Recommended {item.spoilTime} days.</text>
+        <text className="Default-time">Default {item.defaultTime} days.</text>
+      </p>
+    </div>
+  );
+};
 
 const Recommendations = () => (
   <div>
-    <NavBar/>
+    <NavBar />
     <header className="App-header">
       <h1>Yuh</h1>
     </header>
---
+++ front-end/src/StorageTimeSearchListGroups.js
@@ -0,0 +1,136 @@
+import React from "react";
+import { StorageTimeSearchList, Item } from "./App";
+const StorageTimeSearchListFruits = () => {
+  const items = [
+    {
+      id: 1,
+      name: "Apple",
+      spoilTime: 3,
+      defaultTime: 1,
+    },
+    {
+      id: 2,
+      name: "Pear",
+      spoilTime: 5,
+      defaultTime: 1,
+    },
+    {
+      id: 3,
+      name: "Grapes",
+      spoilTime: 10,
+      defaultTime: 2,
+    },
+  ];
+  const itemList = items.map((item) => <Item key={item.id} item={item}></Item>);
+  return (
+    <div>
+      <header className="App-header">
+        <StorageTimeSearchList title="Fruits" />
+        {itemList}
+      </header>
+    </div>
+  );
+};
+
+const StorageTimeSearchListMeats = () => {
+  const items = [
+    {
+      id: 1,
+      name: "Chicken",
+      spoilTime: 3,
+      defaultTime: 1,
+    },
+    {
+      id: 2,
+      name: "Pork",
+      spoilTime: 5,
+      defaultTime: 1,
+    },
+    {
+      id: 3,
+      name: "Beef",
+      spoilTime: 10,
+      defaultTime: 2,
+    },
+  ];
+  const itemList = items.map((item) => <Item key={item.id} item={item}></Item>);
+  return (
+    <div>
+      <header className="App-header">
+        <StorageTimeSearchList title="Meats" />
+        {itemList}
+      </header>
+    </div>
+  );
+};
+
+const StorageTimeSearchListDairy = () => {
+  const items = [
+    {
+      id: 1,
+      name: "Milk",
+      spoilTime: 3,
+      defaultTime: 1,
+    },
+    {
+      id: 2,
+      name: "Ice cream",
+      spoilTime: 5,
+      defaultTime: 1,
+    },
+    {
+      id: 3,
+      name: "Cheese",
+      spoilTime: 10,
+      defaultTime: 2,
+    },
+  ];
+  const itemList = items.map((item) => <Item key={item.id} item={item}></Item>);
+  return (
+    <div>
+      <header className="App-header">
+        <StorageTimeSearchList title="Meats" />
+        {itemList}
+      </header>
+    </div>
+  );
+};
+
+const StorageTimeSearchListGrain = () => {
+  const items = [
+    {
+      id: 1,
+      name: "Whole wheat bread",
+      spoilTime: 3,
+      defaultTime: 1,
+    },
+    {
+      id: 2,
+      name: "Oatmeal",
+      spoilTime: 5,
+      defaultTime: 1,
+    },
+    {
+      id: 3,
+      name: "Rice",
+      spoilTime: 10,
+      defaultTime: 2,
+    },
+  ];
+  const itemList = items.map((item) => <Item key={item.id} item={item}></Item>);
+  return (
+    <div>
+      <header className="App-header">
+        <StorageTimeSearchList title="Grain" />
+        {itemList}
+      </header>
+    </div>
+  );
+};
+
+export {
+  StorageTimeSearchListFruits,
+  StorageTimeSearchListDairy,
+  StorageTimeSearchListMeats,
+  StorageTimeSearchListGrain,
+};
