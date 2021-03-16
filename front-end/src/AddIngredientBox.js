66489ffe111029af6aa3369052db9f6e5ff2428b
Ziyi <wangziyi71600@gmail.com>
2021-03-16 19:52:11 +0800

    recipe page modification

--- front-end/src/AddIngredientBox.js
+++ front-end/src/AddIngredientBox.js
@@ -2,12 +2,33 @@ import React from "react";
 import "./AddIngredientBox.css";
 
 const AddIngredientBox = (props) => {
+  const myItems=[{
+    id:1,
+    title:"egg",
+
+  },
+  {
+    id:2,
+    title:"banana",
+  },
+]
+const itemList=myItems.map((myItems)=>{return myItems.title})
+if (itemList.includes(props.ingredient.name)){
   return (
     <div class="box">
-      <p>{props.ingredient.amount + " " + props.ingredient.name}</p>
+      <p>{props.ingredient.amount + " "} <span class="haveIt">{props.ingredient.name}</span> </p>
       <button class="ingredientButton">+</button>
     </div>
   );
-};
+  
+}
+else {
+  return (
+    <div class="box">
+      <p>{props.ingredient.amount + " "}<span style={{color:"red"}}>{props.ingredient.name}</span> </p>
+      <button class="ingredientButton">+</button>
+    </div>
+  )
+}}
 
-export default AddIngredientBox;
+export default AddIngredientBox
