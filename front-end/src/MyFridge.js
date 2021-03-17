d22d90a113a279b5f75969bdca77939a72d3e5ca
Alex <ah4605@stern.nyu.edu>
2021-03-17 18:25:07 -0400

    revert Delete functionality

--- front-end/src/MyFridge.js
+++ front-end/src/MyFridge.js
@@ -1,14 +1,71 @@
-import React from "react"
+import React, { useState } from "react";
 import "./MyFridge.css";
 import { groups } from "./fridgeDataFake";
-import MyFridgeItem from "./MyFridgeItem";
+import { dot, chipDays, chipAmount } from "./itemColoring";
+import Modal from "./deleteModal";
 
 const MyFridge = (props) => {
+  const [show, setShow] = useState(false);
+  const [itemName, setItemName] = useState("Hello");
+  const [itemId, setItemId] = useState(0);
 
-  const renderItem = (data) => {
-    return <MyFridgeItem key={data.id} food={data} />;
+  const onDelete = (itemInput, groups) => {
+    let matchIndex = parseInt(itemId);
+    for (let i = 0; i < groups.length; i++) {
+      var removeIndex = groups[i].object
+        .map(function (item) {
+          return item.id;
+        })
+        .indexOf(matchIndex);
+      if (removeIndex !== -1) {
+        groups[i].object.splice(removeIndex, 1);
+      }
+    }
   };
 
+  const renderItem = (data, j) => {
+    const rowEvent = (event) => {
+      const title = event.currentTarget.getAttribute("title");
+      const id = event.currentTarget.getAttribute("id");
+      // https://stackoverflow.com/questions/43335452/pass-item-data-to-a-react-modal
+      setItemName(title);
+      setItemId(id);
+      setShow(true);
+    };
+
+    // dot, chipAmount, and chipDays can be found in itemColoring.js
+    return (
+      <tbody key={j}>
+        <tr>
+          <td>
+            <span>{dot(data.daysleft)}</span>
+            <span className="title">{data.title}</span>
+            <span>{chipAmount(data.amount, data.daysleft)}</span>
+            <span>{chipDays(data.daysleft)}</span>
+          </td>
+          <td>
+            <button
+              title={data.title}
+              id={data.id}
+              amount={data.amount}
+              daysleft={data.daysleft}
+              onClick={rowEvent}
+            >
+              x
+            </button>
+          </td>
+        </tr>
+        <Modal
+          onClose={() => setShow(false)}
+          show={show}
+          onDelete={() => onDelete(data, groups)}
+          itemName={itemName}
+        />
+      </tbody>
+    );
+  };
+
+  // groups is an object in fridgeDataFake.js
   return (
     <div>
       {groups.map((item, i) => (
@@ -21,4 +78,5 @@ const MyFridge = (props) => {
   );
 };
 
+// make this available to other modules as an import
 export default MyFridge;
