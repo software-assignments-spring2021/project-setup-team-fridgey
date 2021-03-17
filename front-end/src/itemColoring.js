73ac81d7a8a8736920f49f272d2d163d8d081896
Alex <ah4605@stern.nyu.edu>
2021-03-16 23:58:11 -0400

    Trying to Resolve Merge Conflict with ItemColoring.js

--- front-end/src/itemColoring.js
+++ front-end/src/itemColoring.js
@@ -1,74 +1,78 @@
 import React from "react";
 
+// the css page for this is MyFridge.css
+
+// these are all functions!!
 export const dot = (days) => {
-  return (
-    <span
-      className={`dot ${
-        days < 2 ? "dotRed" : days >= 2 && days < 6 ? "dotYellow" : "dotGreen"
-      }`}
-    ></span>
-  );
+  if (days < 2) {
+    return <span className="dot dotRed"></span>;
+  } else if (days >= 2 && days < 6) {
+    return <span className="dot dotYellow"></span>;
+  } else {
+    return <span className="dot dotGreen"></span>;
+  }
 };
 
 export const chipDays = (days) => {
-  return (
-    <p
-      className={`chip chipFilled ${
-        days < 2
-          ? "chipRed"
-          : days >= 2 && days < 6
-          ? "chipYellow"
-          : "chipGreen"
-      }`}
-    >
-      {days === 0 ? "Throw Out" : days + " Days"}{" "}
-    </p>
-  );
+  if (days < 2) {
+    return <p className="chip chipFilled chipRed"> {days + " Days"}</p>;
+  } else if (days >= 2 && days < 6) {
+    return <p className="chip chipFilled chipYellow"> {days + " Days"}</p>;
+  } else {
+    return <p className="chip chipFilled chipGreen"> {days + " Days"}</p>;
+  }
 };
 
-export const chipAmount = (amount, days) => {
-  return (
-    <p
-      className={`chip ${
-        days === 0
-          ? "noChip"
-          : amount === "Lots"
-          ? "chipLots"
-          : amount === "Some"
-          ? "chipSome"
-          : "chipFew"
-      }`}
-    >
-      {amount}
-    </p>
-  );
+export const chipAmount = (amount) => {
+  if (amount === "Lots") {
+    return <p className="chip chipLots"> {amount}</p>;
+  } else if (amount === "Some") {
+    return <p className="chip chipSome"> {amount}</p>;
+  } else {
+    return <p className="chip chipFew"> {amount}</p>;
+  }
 };
 
-/* IF ELSE VERSION */
-// if (days < 2) {
-//   return <span class="dot dotRed"></span>;
-// } else if (days >= 2 && days < 6) {
-//   return <span class="dot dotYellow"></span>;
-// } else {
-//   return <span class="dot dotGreen"></span>;
-// }
-
-/* IF ELSE VERSION */
-// if (days < 2) {
+// export const dot = (days) => {
 //   return (
-//     <p class="chip chipFilled chipRed"> {days === 0 ? "Throw Out" : days + " Days"} </p>
+//     <span
+//       className={`dot ${
+//         days < 2 ? "dotRed" : days >= 2 && days < 6 ? "dotYellow" : "dotGreen"
+//       }`}
+//     ></span>
 //   );
-// } else if (days >= 2 && days < 6) {
-//   return <p class="chip chipFilled chipYellow"> {days + " Days"}</p>;
-// } else {
-//   return <p class="chip chipFilled chipGreen"> {days + " Days"}</p>;
-// }
+// };
 
-/* IF ELSE VERSION */
-// if (amount === "Lots") {
-//   return <p class="chip chipLots"> {amount}</p>;
-// } else if (amount === "Some") {
-//   return <p class="chip chipSome"> {amount}</p>;
-// } else {
-//   return <p class="chip chipFew"> {amount}</p>;
-// }
+// export const chipDays = (days) => {
+//   return (
+//     <p
+//       className={`chip chipFilled ${
+//         days < 2
+//           ? "chipRed"
+//           : days >= 2 && days < 6
+//           ? "chipYellow"
+//           : "chipGreen"
+//       }`}
+//     >
+//       {days === 0 ? "Throw Out" : days + " Days"}{" "}
+//     </p>
+//   );
+// };
+
+// export const chipAmount = (amount, days) => {
+//   return (
+//     <p
+//       className={`chip ${
+//         days === 0
+//           ? "noChip"
+//           : amount === "Lots"
+//           ? "chipLots"
+//           : amount === "Some"
+//           ? "chipSome"
+//           : "chipFew"
+//       }`}
+//     >
+//       {amount}
+//     </p>
+//   );
+// };
