c34258a34f5c285548dbecf173f3d6466cf7e121
Kevin Ho <kh2576@nyu.edu>
2021-03-19 04:36:21 -0400

    edited recommendations for more reuseability

--- front-end/src/Recommendations/Recommendations.js
+++ front-end/src/Recommendations/Recommendations.js
@@ -7,15 +7,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { recipes } from "../data/recommendationDataFake";
-import {ReadyToMake} from "./ReadyToMake";
-import {SavedRecipes} from "./SavedRecipes";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      paddingBottom: 20,
-    },
+export const useStyles = makeStyles((theme) => ({
     paper: {
       padding: theme.spacing(1),
       margin: 'auto',
@@ -29,59 +22,81 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-const Recommendations = (props) => {
+export const RecItem = (data) => {
   const classes = useStyles();
-
-  
-
-  const RecItem = (data) => {
-    const [buttonText,setButtonText] = useState('Save Recipe')
-    return(
-      <div className={classes.root}>
-          <Grid container spacing={0} >
-              <Grid item xs = {5}>
-                  <ButtonBase>
-                      <img className= "recommendations-imagePreview" alt="complex" src={data.image} />
-                  </ButtonBase>
-              </Grid>
-              <Grid item xs = {6}>
-                  <div className = "recommendations-recipeTitle">{data.title}</div>
-                  <div className = "recommendations-info">{data.ingredients} Ingredients | {data.minutes} Minutes</div>
-                  <Button className = "recommendations-addButton" variant = "outlined" color="primary" onClick={() => setButtonText('Saved!')}> {buttonText} </Button>
-              </Grid> 
-          </Grid>
-    </div>
-    );
+  const [buttonText,setButtonText] = useState('Save Recipe')
+  return(
+    <div className="recommendations-root">
+        <Grid container spacing={0} >
+            <Grid item xs = {5}>
+                <ButtonBase>
+                    <img className= "recommendations-imagePreview" alt="complex" src={data.image} />
+                </ButtonBase>
+            </Grid>
+            <Grid item xs = {6}>
+                <div className = "recommendations-recipeTitle">{data.title}</div>
+                <div className = "recommendations-info">{data.ingredients} Ingredients | {data.minutes} Minutes</div>
+                <Button className = "recommendations-addButton" variant = "outlined" color="primary" onClick={() => setButtonText('Saved!')}> {buttonText} </Button>
+            </Grid> 
+        </Grid>
+  </div>
+  );
 };
 
+export const HeaderButtons = (data) => {
+  return(
+    <div>
+      <Link to = "/Recommendations" className = "recommendations-link">
+        <Button className = {data.first}>
+          Recipes of the Day
+        </Button>
+      </Link>
+        <Link to = "/ReadyToMake" className = "recommendations-link">
+          <Button className = {data.second}>
+            Ready to Make
+          </Button>
+        </Link>
+        <Link to = "/SavedRecipes" className = "recommendations-link">
+          <Button className = {data.third}>
+              Saved Recipes
+          </Button>
+        </Link>
+      </div>
+  );
+};
+
+export const GeneratePaper = ({children}) => {
+  const classes = useStyles();
+  return(
+    <Paper className={classes.paper}>
+      {children}
+    </Paper>
+  );
+};
+
+const GenerateData = (data) => {
+  return(
+    <div>
+      <RecItem title = "Classic Lasagna" ingredients = "7" minutes = "60" image = "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg"/>
+      <RecItem title = "Korean Pork Chops" ingredients = "7" minutes = "60" image = "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg"/>
+      <RecItem title = "Chicken Marsala" ingredients = "7" minutes = "60" image = "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg"/>
+      <RecItem title = "Classic Lasagna" ingredients = "7" minutes = "60" image = "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg"/>
+      <RecItem title = "Korean Pork Chops" ingredients = "7" minutes = "60" image = "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg"/>
+      <RecItem title = "Chicken Marsala" ingredients = "7" minutes = "60" image = "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg"/>
+    </div>
+  );
+};
+
+const Recommendations = (props) => {
   return(
   <div>
     <NavBar/>
     <header className="App-header">
-      
-    <Link to = "/Recommendations" className = "recommendations-link">
-      <Button className = "recommendations-usedButton">
-        Recipes of the Day
-      </Button>
-    </Link>
-      <Link to = "/ReadyToMake" className = "recommendations-link">
-        <Button className = "recommendations-unusedButton">
-          Ready to Make
-        </Button>
-      </Link>
-      <Link to = "/SavedRecipes" className = "recommendations-link">
-        <Button className = "recommendations-unusedButton">
-            Saved Recipes
-        </Button>
-      </Link>  
-      <Paper className={classes.paper}>
-          <RecItem title = "Classic Lasagna" ingredients = "7" minutes = "60" image = "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg"/>
-          <RecItem title = "Korean Pork Chops" ingredients = "7" minutes = "60" image = "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg"/>
-          <RecItem title = "Chicken Marsala" ingredients = "7" minutes = "60" image = "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg"/>
-          <RecItem title = "Classic Lasagna" ingredients = "7" minutes = "60" image = "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg"/>
-          <RecItem title = "Korean Pork Chops" ingredients = "7" minutes = "60" image = "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg"/>
-          <RecItem title = "Chicken Marsala" ingredients = "7" minutes = "60" image = "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg"/>
-      </Paper>
+      <h1>Recommendations</h1>
+      <HeaderButtons first = "recommendations-usedButton" second = "recommendations-unusedButton" third = "recommendations-unusedButton"/>
+      <GeneratePaper>
+        <GenerateData/>
+      </GeneratePaper>
     </header>
   </div> 
 );
