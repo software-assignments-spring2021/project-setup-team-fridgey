2d965ecba0c88de29b77cf99101f18da29782205
Alex <ah4605@stern.nyu.edu>
2021-03-20 02:28:56 -0400

    Edited MyFridge to take in JSON Data

--- front-end/src/MyFridge/CountFridgeItems.js
+++ front-end/src/MyFridge/CountFridgeItems.js
@@ -1,10 +1,10 @@
 import { groups } from "../data/fridgeDataFake";
-
+const fridgeData = require("../data/fridgeMockData.json");
 export let num;
 export const itemCount = () => {
   num = 0;
-  for (let i = 0; i < groups.length; i++) {
-    for (let j = 0; j < groups[i].category.length; j++) {
+  for (let i = 0; i < Object.entries(fridgeData[0]).length; i++) {
+    for (let j = 0; j < Object.entries(fridgeData[0])[i][1].length; j++) {
       num = num + 1;
     }
   }
