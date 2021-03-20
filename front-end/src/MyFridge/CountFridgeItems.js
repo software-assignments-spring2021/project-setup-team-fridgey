c8101860c1447d3bdd41db6cf2df62f7e1f8a4b4
Alex <ah4605@stern.nyu.edu>
2021-03-20 14:19:41 -0400

    Cleaned up code

--- front-end/src/MyFridge/CountFridgeItems.js
+++ front-end/src/MyFridge/CountFridgeItems.js
@@ -1,9 +1,11 @@
-import { groups } from "../data/fridgeDataFake";
+// import { groups } from "../data/fridgeDataFake";
 const fridgeData = require("../data/fridgeMockData.json");
 export let num;
 export const itemCount = () => {
   num = 0;
+  // fridgeData[0] is 1 large object that contains 4 Keys (Fruits, Grains etc.) inside of it that we turn into an Array entries
   for (let i = 0; i < Object.entries(fridgeData[0]).length; i++) {
+    // Each Array Entries has 2 values, the Object key (eg Fruit), and then the List of objects (eg. items in Fruits)
     for (let j = 0; j < Object.entries(fridgeData[0])[i][1].length; j++) {
       num = num + 1;
     }
