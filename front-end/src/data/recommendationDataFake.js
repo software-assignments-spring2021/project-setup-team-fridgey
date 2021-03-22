9959fa326632182cb5ad19df92d59fea161bf30f
Kevin Ho <kh2576@nyu.edu>
2021-03-21 22:21:33 -0400

    added map function to recs pages to pull from array of recipes

--- front-end/src/data/recommendationDataFake.js
+++ front-end/src/data/recommendationDataFake.js
@@ -1,39 +1,39 @@
-export const recipes = [
-    {
-      title: "Classic Lasagna",
+export const recommendationData = 
+[
+  {
+    title: "Classic Lasagna",
+    ingredients: "7",
+    minutes: 60,
+    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
+  },
+  {
+    title: "Koran Pork Chops",
+    ingredients: "7",
+    minutes: 60,
+    image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
+  },
+  {
+    title: "Chicken Marsala",
+    ingredients: "13",
+    minutes: 55,
+    image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
+  },
+  {
+    title: "Low Carb Skillet",
+    ingredients: "7",
+    minutes: 60,
+    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
+  },
+  {
+      title: "Indian Cauliflower",
       ingredients: "7",
       minutes: 60,
-      image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-    },
-    {
-      title: "Korean Pork Chops",
+      image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
+  },
+  {
+      title: "Oven-Baked Chicken",
       ingredients: "7",
       minutes: 60,
-      image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
+      image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
     },
-    {
-      title: "Chicken Marsala",
-      ingredients: "13",
-      minutes: 55,
-      image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-    },
-    {
-      title: "Low Carb Skillet",
-      ingredients: "7",
-      minutes: 60,
-      image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-    },
-    {
-        title: "Indian Butter Cauliflower",
-        ingredients: "7",
-        minutes: 60,
-        image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-    },
-    {
-        title: "Oven Baked Chicken",
-        ingredients: "7",
-        minutes: 60,
-        image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-      },
-  ];
-  
+];
