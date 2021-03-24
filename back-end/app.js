7d2d1dbd33e9b8f247d21f3dedfc1356e39ef489
JLin555 <joshualin555@gmail.com>
2021-03-24 13:48:08 -0700

    Added default routes

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,18 @@ const express = require("express") // CommonJS import style!
 const app = express() // instantiate an Express object
 
 // we will put some server logic here later...
+app.get("/", (req, res) => {
+  res.send("Hello!")
+})
+
+app.get("/json-example", (req, res) => {
+  const body = {
+    title: 'Hello',
+    heading:"Hello",
+    message: "Alex is sexy",
+  }
+  res.json(body)
+})
 
 // export the express app we created to make it available to other modules
 module.exports = app
--- back-end/server.js
+++ back-end/server.js
@@ -6,14 +6,14 @@ const port = 3000 // the port to listen to for incoming requests
 
 // call express's listen function to start listening to the port
 const listener = server.listen(port, function () {
- console.log(`Server running on port: ${port}`)
+  console.log(`Server running on port: ${port}`)
 })
 
 // a function to stop listening to the port
 const close = () => {
- listener.close()
+  listener.close()
 }
 
 module.exports = {
- close: close,
+  close: close,
 }
