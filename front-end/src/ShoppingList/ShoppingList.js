f9bb04fc2543564fc1c5c635c36739060ea59184
Alex <ah4605@stern.nyu.edu>
2021-03-24 15:28:42 -0400

    Created welcome message and starter code for table

--- front-end/src/ShoppingList/ShoppingList.js
+++ front-end/src/ShoppingList/ShoppingList.js
@@ -1,11 +1,52 @@
 import React, { useState } from "react";
 import NavBar from "../NavBar";
+import welcome_pic from "../MyFridge/MyFridge-Welcome-Pic.png";
+import "./ShoppingList.css";
+
+const ShoppingListView = (props) => {
+  const renderItem = () => {
+    return (
+      <tbody>
+        <tr>
+          <td>
+            <span>Apple</span>
+          </td>
+          <td>
+            <button>x</button>
+          </td>
+        </tr>
+      </tbody>
+    );
+  };
+
+  let a = false;
+  return (
+    <div>
+      <div className={a ? "ShoppingList-Hide" : ""}>
+        <table>{renderItem()}</table>
+      </div>
+      <div className={a ? "" : "ShoppingList-Hide"}>
+        <h2> Welcome to Your Shopping List!</h2>
+        <img
+          src={welcome_pic}
+          alt="MyFridge-Welcome"
+          width="300"
+          height="270"
+        />
+        <p className="ShoppingList-Welcome-Msg">
+          Start adding items using the add button! :)
+        </p>
+      </div>
+    </div>
+  );
+};
 // the home page with the items and the stuff at the bottom
 const ShoppingList = () => (
   <div>
     <NavBar />
     <header className="App-header">
       <h1>Shopping List</h1>
+      <ShoppingListView />
     </header>
   </div>
 );
