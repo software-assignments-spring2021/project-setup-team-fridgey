5ca6fceb94bd69571f09316d9c6ee50e53f9296d
Kevin Ho <kh2576@nyu.edu>
2021-03-26 00:56:27 -0400

    keep product titles on one line by trimming end of 20 characters

--- front-end/src/Recommendations/ReadyToMake.js
+++ front-end/src/Recommendations/ReadyToMake.js
@@ -2,55 +2,7 @@ import NavBar from "../NavBar";
 import "./Recommendations.css";
 import {GeneratePaper} from "./Recommendations"
 import {HeaderButtons} from "./Recommendations"
-import {RecItem} from "./Recommendations"
-
-const recommendationData = 
-[
-  {
-    title: "Classic Lasagna",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-  },
-  {
-    title: "Koran Pork Chops",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
-  },
-  {
-    title: "Marsala",
-    ingredients: "13",
-    minutes: 55,
-    image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
-  },
-  {
-    title: "Low Carb Skillet",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-  },
-  {
-      title: "Indian Cauliflower",
-      ingredients: "7",
-      minutes: 60,
-      image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
-  },
-  {
-      title: "Oven-Baked Chicken",
-      ingredients: "7",
-      minutes: 60,
-      image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
-    },
-];
-
-const GenerateData = (data) => {
-  return(
-    recommendationData.map((recipe) => (
-      <RecItem title = {recipe.title} ingredients = {recipe.ingredients} minutes = {recipe.minutes} image = {recipe.image}/>
-    )
-  ));
-};
+import {GenerateData} from "./Recommendations"
 
 const ReadyToMake = (props) => {
   return(
