d2fe0bc11525fbb4105c00fe21402df7daab9879
Kevin Ho <kh2576@nyu.edu>
2021-03-31 01:35:17 -1000

    testing backend route

--- front-end/src/Recommendations/ReadyToMake.js
+++ front-end/src/Recommendations/ReadyToMake.js
@@ -3,51 +3,21 @@ import "./Recommendations.css";
 import {GeneratePaper} from "./Recommendations"
 import {HeaderButtons} from "./Recommendations"
 import {RecItem} from "./Recommendations"
-
-const recommendationData = 
-[
-  {
-    title: "Classic Lasagna",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-  },
-  {
-    title: "Koran Pork Chops",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
-  },
-  {
-    title: "Marsala",
-    ingredients: "13",
-    minutes: 55,
-    image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
-  },
-  {
-    title: "Low Carb Skillet",
-    ingredients: "7",
-    minutes: 60,
-    image: "https://www.thewholesomedish.com/wp-content/uploads/2018/07/Best-Lasagna-550.jpg",
-  },
-  {
-      title: "Indian Cauliflower",
-      ingredients: "7",
-      minutes: 60,
-      image: "https://www.jocooks.com/wp-content/uploads/2015/03/korean-style-pork-chops-2.jpg",
-  },
-  {
-      title: "Oven-Baked Chicken",
-      ingredients: "7",
-      minutes: 60,
-      image: "https://www.onceuponachef.com/images/2018/01/Chicken-Marsala.jpg",
-    },
-];
+import { useState,useEffect } from "react";
+import axios from 'axios';
 
 const GenerateData = (data) => {
+  const [recipeData,setRecipeData] = useState([]);
+  useEffect(() => {
+    axios.get("localhost:3000/testRecs").then(response => {
+      setRecipeData(response.data)
+    });
+    
+  },[]);
+
   return(
-    recommendationData.map((recipe) => (
-      <RecItem title = {recipe.title} ingredients = {recipe.ingredients} minutes = {recipe.minutes} image = {recipe.image}/>
+    recipeData.map((recipe) => (
+      <RecItem title = {recipe.name} ingredients = {recipe.ingredients.length} minutes = {recipe.time} image = {recipe.imageURL}/>
     )
   ));
 };
