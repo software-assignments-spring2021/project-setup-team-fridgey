b3f1614e10cabbf8847e9ba5ab2c9b1d37e2ddc0
Javier Saldana <saldanaj27@gmail.com>
2021-04-01 13:52:31 -0400

    can now add items based on what is selected and written on modal

--- front-end/src/ShoppingList/AddNewFridgeItemModal.js
+++ front-end/src/ShoppingList/AddNewFridgeItemModal.js
@@ -2,13 +2,23 @@ import { React } from "react";
 import "./AddNewFridgeItemModal.css";
 
 const AddNewFridgeItemModal = (props) => {
+    var pressedAmount = "Lots"
+
+    const changeData = () => {
+        var pressed = document.getElementById("NewItemAmount-chips").getElementsByClassName("chip pressed")
+        var unpressed = document.getElementById("NewItemAmount-chips").getElementsByClassName("chip unpressed")
+
+        console.log(pressed)
+        console.log("HAHS")
+        console.log(unpressed)
+    }
 
     const grabInformation = (event) => {
         var itemName = document.getElementById("itemName").value;
-        var amountElements = document.getElementById("NewItemAmount-chips").getElementsByClassName("chip pressed")
-        var amount = amountElements[0].innerHTML
-        var typeElements = document.getElementById("NewItemType-chips").getElementsByClassName("chip pressed")
-        var type = typeElements[0].innerHTML
+        var amount = document.getElementById("NewItemAmount-chips").getElementsByClassName("chip pressed")[0].innerHTML
+        // var amount = amountElements[0].innerHTML
+        var type = document.getElementById("NewItemType-chips").getElementsByClassName("chip pressed")[0].innerHTML
+        // var type = typeElements[0].innerHTML
 
         if(type === "Fruits") {
             type = 0;
@@ -26,8 +36,6 @@ const AddNewFridgeItemModal = (props) => {
 
     const onTrigger = (name, amount, type) => {
         props.parentCallback(name, amount, type)
-        
-        // event.preventDefault();
     }
 
     return (
@@ -48,9 +56,24 @@ const AddNewFridgeItemModal = (props) => {
                         <h5 className="NewFridgeItemModal-text">How Much?</h5>
 
                         <div id="NewItemAmount-chips">
-                            <button className={`chip chip-Lots pressed`}>Lots</button>
-                            <button className={`chip chip-Some`}>Some</button>
-                            <button className={`chip chip-Few`}>Few</button>
+                            <button 
+                                onClick={changeData} 
+                                className={`chip chip-Lots ${(pressedAmount === "Lots") ? "pressed" : "unpressed"}`}
+                            >
+                            Lots
+                            </button>
+                            <button 
+                                onClick={changeData}
+                                className={`chip chip-Some ${(pressedAmount === "Some") ? "pressed" : "unpressed"}`}
+                            >
+                            Some
+                            </button>
+                            <button
+                                onClick={changeData} 
+                                className={`chip chip-Few ${(pressedAmount === "Few") ? "pressed" : "unpressed"}`}
+                            >
+                            Few
+                            </button>
                         </div>
                     </div>
 
