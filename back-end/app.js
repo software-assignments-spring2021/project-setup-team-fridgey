565fdb1847b88d43d3f176f4f50454a7542ac563
Alex <ah4605@stern.nyu.edu>
2021-04-02 12:44:52 -0400

    Created Initial Get Request

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,43 @@
 // import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
+const express = require("express"); // CommonJS import style!
+const app = express(); // instantiate an Express object
+// const port = 5000;
+var request = require("request");
 
+const data = require("../front-end/src/data/fridgeMockData.json");
+console.log(data);
 // we will put some server logic here later...
 app.get("/", (req, res) => {
-  res.send("Hello!")
-})
+  res.send("Hello!");
+});
 
 app.get("/json-example", (req, res) => {
   const body = {
-    title: 'Hello',
-    heading:"Hello",
+    title: "Hello",
+    heading: "Hello",
     message: "Alex is sexy",
-  }
-  res.json(body)
-})
+  };
+  res.json(body);
+});
+
+app.get("/getFridgeData", (req, res) => {
+  // request(
+  //   "https://my.api.mockaroo.com/fridge_mock_data.json?key=842e8840",
+  //   function (error, response, body) {
+  //     if (!error && response.statusCode == 200) {
+  //       // var parsedBody = JSON.parse(body);
+  //       // var a = parsedBody[0]["meats"];
+  //       res.send(body);
+  //     }
+  //   }
+  // );
+  res.json(data);
+  // let b = await axios.get("/getFridgeData");
+  // console.log(b.data[0]);
+  // return Object.entries(b.data[0]);
+});
+
+// app.listen(port, () => console.log("Hello!"));
 
 // export the express app we created to make it available to other modules
-module.exports = app
+module.exports = app;
