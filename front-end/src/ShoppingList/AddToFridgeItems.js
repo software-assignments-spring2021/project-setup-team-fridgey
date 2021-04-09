2f2936a507377ddc5899591b5eea0bd0da8b2c1b
cindykimxp <cindykim100@gmail.com>
2021-04-08 21:56:51 -0400

    get storagetimes from backend

--- front-end/src/ShoppingList/AddToFridgeItems.js
+++ front-end/src/ShoppingList/AddToFridgeItems.js
@@ -10,7 +10,7 @@ export function getSelectedCheckboxItems(name) {
   return values;
 }
 
-export function compileAddToFridgeItems() {
+export function compileAddToFridgeItems(storageitems) {
   let objects = [];
   // model object
   const foodItem = {
@@ -29,11 +29,30 @@ export function compileAddToFridgeItems() {
     add.title = vals[i].getAttribute("value");
     add.type = vals[i].getAttribute("food");
     add.amount = vals[i].getAttribute("amount");
-    add.dateadded = vals[i].getAttribute("date");
-    add.daysleft = 5; // PLACEHOLDER FOR FUTURE WHEN WE AUTOMATICALLLY PUT IN STORAGE TIME
+    add.dateadded = vals[i].getAttribute("date"); //April 1, 2021
+
+    try{
+      const findItem = storageitems.find(elem => elem.food === add.title)
+      add.daysleft = findItem.storage_time_medium;
+      } catch (e){
+        console.log("doesnt work!")
+        console.log(e);
+        console.log(add.type);
+        if (add.type == 1){
+          add.daysleft = 5;
+        }
+        else if (add.type == 2){
+          add.daysleft = 6;
+        }
+        else if (add.type == 3){
+          add.daysleft = 7;
+        }
+        else {
+          add.daysleft = 8;
+        }
+    }
     objects.push(add);
   }
-
   // returns array of objeccts of all items that are to be added
   return objects;
 }
