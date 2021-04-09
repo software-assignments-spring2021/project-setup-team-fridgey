967e3c2e3f153e4a464aec67d03f3c0163049c9b
cindykimxp <cindykim100@gmail.com>
2021-04-09 00:52:56 -0400

    hardcoded data for now!

--- front-end/src/ShoppingList/AddToFridgeItems.js
+++ front-end/src/ShoppingList/AddToFridgeItems.js
@@ -10,7 +10,58 @@ export function getSelectedCheckboxItems(name) {
   return values;
 }
 
-export function compileAddToFridgeItems(storageitems) {
+export function compileAddToFridgeItems() {
+  const storageitems = [
+    {
+      id: 1,
+      food: "Carbonated Water - Wildberry",
+      storage_time_short: 1,
+      storage_time_medium: 1.5,
+      storage_time_long: 2,
+      category: 2,
+    },
+    {
+      id: 2,
+      food: "Rye Special Old",
+      storage_time_short: 20,
+      storage_time_medium: 30,
+      storage_time_long: 40,
+      category: 3,
+    },
+    {
+      id: 3,
+      food: "Bread - Bagels, Plain",
+      storage_time_short: 37,
+      storage_time_medium: 55.5,
+      storage_time_long: 74,
+      category: 1,
+    },
+    {
+      id: 4,
+      food: "Mix Pina Colada",
+      storage_time_short: 1,
+      storage_time_medium: 1.5,
+      storage_time_long: 2,
+      category: 3,
+    },
+    {
+      id: 5,
+      food: "Bread - White, Unsliced",
+      storage_time_short: 29,
+      storage_time_medium: 43.5,
+      storage_time_long: 58,
+      category: 1,
+    },
+    {
+      id: 6,
+      food: "Beef - Tenderloin Tails",
+      storage_time_short: 15,
+      storage_time_medium: 22.5,
+      storage_time_long: 30,
+      category: 1,
+    },
+  ];
+
   let objects = [];
   // model object
   const foodItem = {
