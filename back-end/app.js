af07e822d52553f759e8cf31d13274afa021d594
Ziyi <wangziyi71600@gmail.com>
2021-04-10 22:06:12 +0800

    pull data from rec page for recipe

--- back-end/app.js
+++ back-end/app.js
@@ -20,19 +20,19 @@ app.use("/fridgeData", MyFridgeRoutes);
 app.use("/shopData", ShoppingListRoutes);
 app.use("/Recommendations", RecommendationsRoutes);
 
-app.get("/getRecipe", (req, res) => {
-  //add :name parameters later
-  request(
-    "https://my.api.mockaroo.com/mock_recipes.json?key=f9883210",
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        let parsedBody = JSON.parse(body);
-        let recipe = parsedBody[0];
-        res.json({ recipe });
-      }
-    }
-  );
-});
+// app.get("/getRecipe", (req, res) => {
+//   //add :name parameters later
+//   request(
+//     "https://my.api.mockaroo.com/mock_recipes.json?key=f9883210",
+//     function (error, response, body) {
+//       if (!error && response.statusCode == 200) {
+//         let parsedBody = JSON.parse(body);
+//         let recipe = parsedBody[0];
+//         res.json({ recipe });
+//       }
+//     }
+//   );
+// });
 
 app.post("/addIngredientToSL", (req, res) => {
   const data = {
