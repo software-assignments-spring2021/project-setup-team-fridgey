8b18ab42cce6da80f0864e703be5fadd6b45e9ca
JLin555 <joshualin555@gmail.com>
2021-04-10 21:54:52 -0700

    Fixed search bar

--- front-end/src/StorageTimeSearch/StorageTimeSearchList.js
+++ front-end/src/StorageTimeSearch/StorageTimeSearchList.js
@@ -4,24 +4,18 @@ import "./StorageTimeSearch.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 // the general structure for a storage time search list
 // includes a back link for every list
 const StorageTimeSearchList = (props) => {
-  const items = [
-    { name: "Apple" },
-    { name: "Pear" },
-    { name: "Grapes" },
-    { name: "Chicken" },
-    { name: "Pork" },
-    { name: "Beef" },
-    { name: "Milk" },
-    { name: "Ice cream" },
-    { name: "Cheese" },
-    { name: "Whole wheat bread" },
-    { name: "Oatmeal" },
-    { name: "Rice" },
-  ];
+  const [items, setItems] = useState(null)
+
+  const axiosResult = axios.get("/storagetimeitems")
+
+  axiosResult.then(response => {
+    setItems(...[response.data])
+  })
 
   const [searchTerm, setSearchTerm] = useState("");
   const [focusSearch, setFocusSearch] = useState("");
@@ -54,29 +48,26 @@ const StorageTimeSearchList = (props) => {
             id="StorageTime-Searchbar"
           />
         </div>
-        {items
-          .filter((item) => {
-            if (searchTerm === "") {
-              return item;
-            } else if (
-              item.name.toLowerCase().includes(searchTerm.toLowerCase())
-            ) {
-              return item;
-            } else {
-              return null;
+        { items.filter((item) => {
+                if (searchTerm === "") {
+                  return item
+                } else if (item.food.toLowerCase().includes(searchTerm.toLowerCase())) {
+                  return item
+                } else {
+                  return null
+                }
+              }).map((item) => {
+                if (searchTerm !== "") {
+                  return (
+                    <div>
+                      <Button>{item.food}</Button>
+                    </div>
+                  );
+                } else {
+                  return (null);
+                }
+              }) 
             }
-          })
-          .map((item) => {
-            if (searchTerm !== "") {
-              return (
-                <div>
-                  <Button>{item.name}</Button>
-                </div>
-              );
-            } else {
-              return <p />;
-            }
-          })}
         <h1 className="food-group">{props.title}</h1>
       </div>
     );
