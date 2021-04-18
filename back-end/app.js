0af164787dbfaaefe618fed50b5a3eac1ac64f26
Ziyi <wangziyi71600@gmail.com>
2021-04-18 17:57:38 +0800

    delete unused routes and change ingredient schema

--- back-end/app.js
+++ back-end/app.js
@@ -20,54 +20,7 @@ app.use("/shopData", ShoppingListRoutes);
 app.use("/Recommendations", RecommendationsRoutes);
 app.use("/storagetimeitems", StorageTimeRoutes);
 
-app.post('/addIngredientToSL', (req, res, next) => {
-  var currentTimeInSeconds=Math.floor(Date.now()/1000);
-  const shoppingList = new ShoppingList({
-    id:"11111",
-    title: req.body.name,
-    amount: "some",
-    type: "????",
-    dataadded:currentTimeInSeconds,
-    notes: "Added from Recipe Page"
-  });
-  shoppingList.save().then(
-    () => {
-      res.status(201).json({
-        message: 'Post saved successfully!'
-      });
 
-      console.log(shoppingList)
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-// app.get("/getRecipe", (req, res) => {
-//   //add :name parameters later
-//   request(
-//     "https://my.api.mockaroo.com/mock_recipes.json?key=f9883210",
-//     function (error, response, body) {
-//       if (!error && response.statusCode == 200) {
-//         let parsedBody = JSON.parse(body);
-//         let recipe = parsedBody[0];
-//         res.json({ recipe });
-//       }
-//     }
-//   );
-// });
-
-// app.post("/addIngredientToSL", (req, res) => {
-//   const data = {
-//     status: "amazing success!",
-//     message: "congratulations on send us this data!",
-//     name: req.body.name,
-//   };
-//   res.json(data);
-// });
-
+  
 // export the express app we created to make it available to other modules
 module.exports = app;
