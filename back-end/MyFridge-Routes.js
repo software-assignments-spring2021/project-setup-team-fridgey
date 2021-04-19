35c49231572b764d3ed74a34a19f29bd8dbbbbdc
Alex <ah4605@stern.nyu.edu>
2021-04-19 12:38:41 -0400

    Cleaned up backend routes for validation

--- back-end/MyFridge-Routes.js
+++ back-end/MyFridge-Routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const FridgeItem = require("./database/fridgeItem");
-const ShopItem = require("./database/shopItem")
+const ShopItem = require("./database/shopItem");
+const { body } = require("express-validator");
 const router = new Router();
 
 // Get Fridge Data
@@ -31,36 +32,46 @@ router.post("/editItem", (req, res) => {
   let editItem = req.body;
 
   // finds the item by _id and updates the item with new amount, new daysleft, and new notes
-  FridgeItem.findByIdAndUpdate(editItem.id,
-    {amount: editItem.amount, daysleft: editItem.useWithin, notes: editItem.notes},
-    {new: true},
-    function(err, docs) {
-      if (err){
-        console.log(err)
-      } else{
+  FridgeItem.findByIdAndUpdate(
+    editItem.id,
+    {
+      amount: editItem.amount,
+      daysleft: editItem.useWithin,
+      notes: editItem.notes,
+    },
+    { new: true },
+    function (err, docs) {
+      if (err) {
+        console.log(err);
+      } else {
         console.log("Updated User : ", docs);
-        res.send(docs)
+        res.send(docs);
       }
     }
-  )
-  
+  );
+
   res.status(200);
 });
 
 // Adds a Specific Fridge Item to Shopping List
-router.post("/addItem", (req, res) => {
-  let addItem = req.body;
-
-  const shopItem = new ShopItem(addItem)
-
-  shopItem.save()
-    .then((result) => {
-      res.send(result)
-      res.status(200);
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-});
+router.post(
+  "/addItem",
+  body("title").isLength({ min: 2 }),
+  body("amount").isLength({ min: 2 }),
+  body("type").isFloat({ min: 0, max: 3 }),
+  (req, res) => {
+    let addItem = req.body;
+    const shopItem = new ShopItem(addItem);
+    shopItem
+      .save()
+      .then((result) => {
+        res.send(result);
+        res.status(200);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+);
 
 module.exports = router;
