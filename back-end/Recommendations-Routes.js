01b2b2c39df3b2067f52e28ef247dbb1969afd82
Kevin Ho <kh2576@nyu.edu>
2021-04-20 05:36:47 -0400

    added 320 recipes to database

--- back-end/Recommendations-Routes.js
+++ back-end/Recommendations-Routes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const recsRoute = require("../front-end/src/data/mock_recipes.json");
+const apiRoute = require("../front-end/src/data/spoonacular_recipes.json");
 const RecipeItem = require('./database/recipeItem');
 const SavedRecipe = require('./database/savedRecipe');
 var savedRecipes = [];
@@ -20,7 +21,7 @@ router.get("/RecipesOfTheDay", (req, res) => {
 
 router.get("/SavedRecipes", async (req, res) => {
   try {
-    retrieveSavedRecipes("6078872528e1ec27a8d057e9").then((result) => {
+    retrieveSavedRecipes("607e927189dc61405c4f6e5a").then((result) => {
       res.json(result);
     })
     res.status(200);
@@ -53,11 +54,11 @@ router.post('/SaveRecipe', async (req, res) => {
     emptyList.save();
   }
   try {
-    var currentSavedList = await SavedRecipe.find({_id: "6078872528e1ec27a8d057e9"});
-    currentSavedList[0].ids = pushIfNotAlreadyExists(req.body._id,currentSavedList[0].ids);
+    var currentSavedList = await SavedRecipe.find({_id: "607e927189dc61405c4f6e5a"});
+    currentSavedList[0].ids = pushIfNotAlreadyExists(req.body.id,currentSavedList[0].ids);
 
     await SavedRecipe.updateOne(
-      {"_id": "6078872528e1ec27a8d057e9"},
+      {"_id": "607e927189dc61405c4f6e5a"},
       { $set: {"ids":currentSavedList[0].ids}}
     );
   } catch (error) {
@@ -74,7 +75,7 @@ async function retrieveSavedRecipes(_id)
   var returnList = [];
   for(var i = 0;i<listOfRecipes.length;i++)
   {
-    await RecipeItem.find({_id: listOfRecipes[i]}).then((result) => {
+    await RecipeItem.find({id: listOfRecipes[i]}).then((result) => {
       returnList.push(result[0]);
     })
   }
@@ -89,18 +90,18 @@ function pushIfNotAlreadyExists(key, currentSavedList)
 
 router.post("/RemoveRecipe", async (req,res) => {
   try {
-    var currentSavedList = await SavedRecipe.find({_id: "6078872528e1ec27a8d057e9"});
+    var currentSavedList = await SavedRecipe.find({_id: "607e927189dc61405c4f6e5a"});
     currentSavedList[0].ids = currentSavedList[0].ids.filter((id) => id != req.body._id);
 
     await SavedRecipe.updateOne(
-      {"_id": "6078872528e1ec27a8d057e9"},
+      {"_id": "607e927189dc61405c4f6e5a"},
       { $set: {"ids":currentSavedList[0].ids}}
     );
   } catch (error) {
       res.status(404);
   }
   try {
-    retrieveSavedRecipes("6078872528e1ec27a8d057e9").then((result) => {
+    retrieveSavedRecipes("607e927189dc61405c4f6e5a").then((result) => {
       res.json(result);
     })
     res.status(200);
@@ -122,5 +123,46 @@ router.get("/add-item", (req,res) => {
     });
   }
 });
+
+router.get("/add-recipes", async (req,res) => {
+  for(var i = 0;i<apiRoute.length;i++)
+  {
+    await parseRecipe(apiRoute[i]);
+  }
+})
+
+async function parseRecipe(recipe)
+{
+  if(await RecipeItem.count({id: recipe.id}) != 0)
+  {
+    console.log("already in database");
+    return;
+  }
+  try{
+    const parsedRecipe = new RecipeItem({
+      id: recipe.id,
+      name: recipe.title,
+      ingredients: await parseIngredients(recipe),
+      time: recipe.readyInMinutes,
+      imageURL: recipe.image,
+      originalURL: recipe.sourceUrl,
+    });
+    await parsedRecipe.save();
+  }
+  catch(error)
+  {
+    return;
+  }
+}
+function parseIngredients(recipe)
+{
+  parsedIngredients = [];
+  for(var i = 0;i<recipe.extendedIngredients.length;i++)
+  {
+    var ingredientsObj = {name: recipe.extendedIngredients[i].original, ingredientName: recipe.extendedIngredients[i].nameClean};
+    parsedIngredients.push(ingredientsObj);
+  }
+  return parsedIngredients;
+}
 
 module.exports = router;
