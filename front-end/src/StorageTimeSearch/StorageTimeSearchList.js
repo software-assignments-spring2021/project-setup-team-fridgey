9a3d05f6f48bb26ab4765dbf527fc12f78ef9b35
JLin555 <joshualin555@gmail.com>
2021-04-19 18:44:23 -0700

    Added modal to storage time search bar results

--- front-end/src/StorageTimeSearch/StorageTimeSearchList.js
+++ front-end/src/StorageTimeSearch/StorageTimeSearchList.js
@@ -21,80 +21,14 @@ const StorageTimeSearchList = (props) => {
     setItems(...[data]);
   };
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const [focusSearch, setFocusSearch] = useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const changeClass = () => {
-    setFocusSearch("StorageTime-Remove");
-  };
-
-  const revertClass = () => {
-    setFocusSearch(null);
-  };
-
-  if (focusSearch === "StorageTime-Remove") {
-    return (
-      <div>
-        <button className="storage-back-button">
-          <Link to="/Storage-Time-Search">Back</Link>
-        </button>
-        <div className="searchlist-search-bar">
-          <input
-            type="text"
-            placeholder="Search for food"
-            onChange={handleChange}
-            onClick={changeClass}
-            onBlur={revertClass}
-            id="StorageTime-Searchbar"
-          />
-        </div>
-        { items.filter((item) => {
-                if (searchTerm === "") {
-                  return item
-                } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-                  return item
-                } else {
-                  return null
-                }
-              }).map((item) => {
-                if (searchTerm !== "") {
-                  return (
-                    <div>
-                      <Button>{item.name}</Button>
-                    </div>
-                  );
-                } else {
-                  return (null);
-                }
-              }) 
-            }
-        <h1 className="food-group">{props.title}</h1>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <button className="storage-back-button">
-          <Link to="/Storage-Time-Search">Back</Link>
-        </button>
-        <div className="searchlist-search-bar">
-          <input
-            type="text"
-            placeholder="Search for food"
-            onChange={handleChange}
-            onClick={changeClass}
-            onBlur={revertClass}
-            id="StorageTime-Searchbar"
-          />
-        </div>
-        <h1 className="food-group">{props.title}</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Button className="storage-back-button">
+        <Link to="/Storage-Time-Search">Back</Link>
+      </Button>
+      <h1 className="food-group">{props.title}</h1>
+    </div>
+  );
 };
 
 export default StorageTimeSearchList;
