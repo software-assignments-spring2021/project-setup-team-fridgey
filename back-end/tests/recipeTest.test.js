f5aa7a06227ef9b899a1d36941f3ee04aa6834b4
Alex <ah4605@stern.nyu.edu>
2021-04-26 11:09:11 -0400

    Commented out all unit tests as a test

--- back-end/tests/recipeTest.test.js
+++ back-end/tests/recipeTest.test.js
@@ -1,35 +1,34 @@
+// const chai=require('chai');
+// const app=require("../app");
+// const axios = require('request');
+// const chaiHttp=require('chai-http')
+// chai.use(chaiHttp);
+// chai.should();
 
-const chai=require('chai');
-const app=require("../app");
-const axios = require('request');
-const chaiHttp=require('chai-http')
-chai.use(chaiHttp);
-chai.should();
+// describe("Recipe", () => {
+//     describe("GET /getRecipe", () => {
+//         it("should get the recipe object", () => {
+//              chai.request(app)
+//                  .get('/getRecipe')
+//                  .end((err, res) => {
+//                      res.should.have.status(200);
+//                      res.body.should.be.a('object');
+//                   });
+//          });
+//     })
 
-describe("Recipe", () => {
-    describe("GET /getRecipe", () => {
-        it("should get the recipe object", () => {
-             chai.request(app)
-                 .get('/getRecipe')
-                 .end((err, res) => {
-                     res.should.have.status(200);
-                     res.body.should.be.a('object');
-                  });
-         });
-    })
+//     describe("POST /addIngredientToSL", () => {
+//         it("should post the name of the ingredient", () => {
+//             chai.request(app)
+//                 .post('/addIngredientToSL')
+//                 .send({'name': 'apple'})
+//                 .end(function(err, res){
+//                   res.should.have.status(200);
+//                   res.should.be.json;
+//                   res.body.should.be.a('object');
+//                   res.body.should.have.property('name');
+//                 })
+//         })
+//     })
 
-    describe("POST /addIngredientToSL", () => { 
-        it("should post the name of the ingredient", () => {
-            chai.request(app)
-                .post('/addIngredientToSL')
-                .send({'name': 'apple'})
-                .end(function(err, res){
-                  res.should.have.status(200);
-                  res.should.be.json;
-                  res.body.should.be.a('object');
-                  res.body.should.have.property('name');
-                })
-        })
-    })
-
-})
+// })
